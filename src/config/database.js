864e15f1849e8cc2f846ff71f056fa357c4fa409
MTardelly <ti.tardelly@gmail.com>
2021-04-08 18:35:57 -0300

    CRUD node.js com relacionamento no sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports={
+    dialect:'mysql',
+    host:'localhost',
+    username: 'root',
+    password:'1234',
+    database:'slqnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
